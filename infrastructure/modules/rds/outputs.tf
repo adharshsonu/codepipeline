edfcbe4f278b035d4636c6db8ef294a3fa8baa39
adharsh.suresh <adharsh.suresh@mallow-tech.com>
2025-10-01 17:14:08 +0530

    deleted infra

--- infrastructure/modules/rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "rds_endpoint" {
-  description = "The hostname of the RDS instance without port"
-  value       = aws_db_instance.this.address
-}
-
-output "rds_port" {
-  description = "The port of the RDS instance"
-  value       = aws_db_instance.this.port
-}
